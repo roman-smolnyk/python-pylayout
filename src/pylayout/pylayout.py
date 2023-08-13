ae8fcf80737ddaa3620157e20c1e02d65a9fadf5
Roman Smolnyk <poma23324@gmail.com>
2023-08-13 20:34:55 +0300

    [feat] Changed windows way to get layouts

--- src/pylayout/pylayout.py
+++ src/pylayout/pylayout.py
@@ -1,25 +1,36 @@
 import json
 import os
+import platform
 import re
+import shlex
 import subprocess
 import sys
+import time
 
-if "win32" in sys.platform:
+if "Windows" in platform.platform():
     import ctypes
     import win32api
+    import win32con
+    import win32gui
+    import win32process
 
     # from ctypes import wintypes
 
 from ._lang_layouts import LAYOUTS
 
 
-class Layout:
+def ban_ruscists(lang: str):
+    if lang == "ru":
+        print("Glory to Ukraine!!!")
+        sys.exit()
 
+
+class Layout:
     _ubuntu_call = "gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval '{command}'"
     _windows_call = "chcp 65001 >NUL & powershell {command}"
 
     def __init__(self) -> None:
-        if "win32" not in sys.platform and "linux" not in sys.platform:
+        if "Windows" not in platform.platform() and "Linux" in platform.platform():
             raise TypeError("Invalid system")
 
         self.cached_layouts = None
@@ -32,18 +43,20 @@ class Layout:
             pass
 
     def get(self) -> str:
-        """Return current layout as 'ru', 'us' etc"""
-        if "win32" in sys.platform:
+        """Return current layout as 'uk', 'us' etc"""
+        if "Windows" in platform.platform():
             if not self.cached_layouts:
                 self.cached_layouts = self._get_available()
-            w = ctypes.windll.user32.GetForegroundWindow()
-            tid = ctypes.windll.user32.GetWindowThreadProcessId(w, 0)
+
+            # Using ctypes as it probably faster but I'm not sure
+            hwnd = ctypes.windll.user32.GetForegroundWindow()
+            tid = ctypes.windll.user32.GetWindowThreadProcessId(hwnd, 0)
             result = ctypes.windll.user32.GetKeyboardLayout(tid)
 
             layouts = {v: k for k, v in self.cached_layouts.items()}
             layout = layouts[result]
             # dictionary[new_key] = dictionary.pop(old_key)
-        elif "linux" in sys.platform:
+        elif "Linux" in platform.platform():
             get_current_layout_command = "imports.ui.status.keyboard.getInputSourceManager().currentSource.id"
             command = self._ubuntu_call.format(command=get_current_layout_command)
             result = self._subprocess_execute(command)
@@ -52,13 +65,16 @@ class Layout:
         # Convert lang names
         layout = "en" if layout == "us" else layout
         layout = "uk" if layout == "ua" else layout
+
+        ban_ruscists(layout)
         return layout
 
     def set(self, dest_lang: str) -> bool:
-        """dest_lang: 'ru', 'us' etc"""
+        """dest_lang: 'uk', 'us' etc"""
+        ban_ruscists(dest_lang)
         if not self.cached_layouts:
             self.cached_layouts = self._get_available()
-        if "win32" in sys.platform:
+        if "Windows" in platform.platform():
             # Cache result to speed up
 
             # w = ctypes.windll.user32.GetForegroundWindow()
@@ -68,21 +84,40 @@ class Layout:
             # lid = int(z["uk"]) & (2**16 - 1)
             # lid_hex = hex(lid)
             # Needs HKL
-            win32api.PostMessage(ctypes.windll.user32.GetForegroundWindow(), 0x0050, 2, self.cached_layouts[dest_lang])
+            # win32api.PostMessage(ctypes.windll.user32.GetForegroundWindow(), 0x0050, 2, self.cached_layouts[dest_lang])
+            code = win32api.PostMessage(
+                win32gui.GetForegroundWindow(),
+                win32con.WM_INPUTLANGCHANGEREQUEST,
+                2,
+                self.cached_layouts[dest_lang],
+            )
+            if not code:
+                return True
+            else:
+                return False
 
             # ActivateKeyboardLayout = ctypes.windll.user32.ActivateKeyboardLayout
             # ActivateKeyboardLayout.argtypes = (wintypes.HKL, wintypes.UINT)
             # ActivateKeyboardLayout.restype = wintypes.HKL
             # ActivateKeyboardLayout(self.available()[dest_lang], 0)
-        elif "linux" in sys.platform:
+        elif "Linux" in platform.platform():
             set_layout_command = f"imports.ui.status.keyboard.getInputSourceManager().inputSources[{self.cached_layouts[dest_lang]}].activate()"
             command = self._ubuntu_call.format(command=set_layout_command)
             result = self._subprocess_execute(command)
+            # TODO Analyze result for success
             return True
+
+    def toggle(self):
+        """Cycle through available layouts"""
+        if "Windows" in platform.platform():
+            win32api.PostMessage(win32gui.GetForegroundWindow(), win32con.WM_INPUTLANGCHANGEREQUEST)
 
     def list(self) -> list:
         """Return list of available layouts"""
-        return list(self._get_available().keys())
+        if not self.cached_layouts:
+            self.cached_layouts = self._get_available()
+        # tuple_ = win32api.GetKeyboardLayoutList()
+        return list(self.cached_layouts.keys())
 
     @staticmethod
     def translate(text: str, source_lang: str, dest_lang: str) -> str:
@@ -119,7 +154,7 @@ class Layout:
 
     def _get_available(self) -> dict:
         layouts = {}
-        if "win32" in sys.platform:
+        if "Windows" in platform.platform():
             # returns KLID
             command = self._windows_call.format(command="Get-WinUserLanguageList")
             result = self._subprocess_execute(command, shell=True)
@@ -142,17 +177,30 @@ class Layout:
             # z._hWnd
 
             # Convert KLID into HKL
-            for k, v in layouts.copy().items():
-                win32api.LoadKeyboardLayout(v.split(":")[1], 1)
-                layouts[k] = win32api.GetKeyboardLayout(0)
-                win32api.LoadKeyboardLayout("00000409", 1)
-        elif "linux" in sys.platform:
+            for i in range(len(layouts.items())):
+                # win32api.LoadKeyboardLayout(v.split(":")[1], win32con.KLF_ACTIVATE)
+                # win32api.GetKeyboardLayout(0)
+                # thread_id = ctypes.windll.user32.GetWindowThreadProcessId(win32gui.GetForegroundWindow(), None)
+                thread_id, process_id = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
+                hkl = win32api.GetKeyboardLayout(thread_id)
+                klid = hex(hkl & 0xFFFFF)
+                # to_hex = hex(hkl)
+                # to_int = int(to_hex, 16)
+                for k, v in layouts.copy().items():
+                    if isinstance(v, str) and klid[-4:] == v.split(":")[0]:
+                        layouts[k] = hkl
+                        break
+                self.toggle()
+                time.sleep(0.1)  # Need timeout to change layout
+            pass
+        elif "Linux" in platform.platform():
             get_layouts_command = "imports.ui.status.keyboard.getInputSourceManager().inputSources"
             command = self._ubuntu_call.format(command=get_layouts_command)
             result = self._subprocess_execute(command)
             result_dict = json.loads(re.findall(r"\{.*\}", result)[0])
             for key, value in result_dict.items():
                 layouts[value["id"]] = int(key)
+            pass
 
         # Convert lang names and persist order
         adapted_layouts = {}
@@ -160,11 +208,14 @@ class Layout:
             key = "en" if key == "us" else key
             key = "uk" if key == "ua" else key
             adapted_layouts[key] = value
+
         return adapted_layouts
 
     def _subprocess_execute(self, command, shell=False):
-        if isinstance(command, str):
-            command = command.split()
+        if isinstance(command, list):
+            pass
+        elif isinstance(command, str):
+            command = shlex.split(command)
         process = subprocess.Popen(command, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output, _ = process.communicate()
         return output.decode()
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 from os.path import dirname as up
 
 path = up(up(os.path.abspath(__file__)))
@@ -10,12 +11,21 @@ from src.pylayout import Layout
 
 def test():
     layout = Layout()
-    print(layout.get())
-    layout.set("uk")
-    print(layout.list())
-    print(layout.detect_language("ё"))
-    print(layout.translate("ghbdsn", "en", "uk"))
-    print(layout.get())
+    lang = layout.get()
+    print("Initial layout:", lang)
+    print("Layout after initialization:", layout.get())
+    print("List of languages:", layout.list())
+    print("Set lang to 'uk':", layout.set("uk"))
+    print("Detect language for 'ї':", layout.detect_language("ї"))
+    print("Translate 'ghbdsn' from 'en' to 'uk':", layout.translate("ghbdsn", "en", "uk"))
+    lang1 = layout.get()
+    print("Current layout:", lang1)
+    assert lang == "uk" or lang != lang1, "Languages are the same"
+    print(f"Set lang to {lang}:", layout.set(lang))
+    time.sleep(0.1)
+    lang2 = layout.get()
+    print("Current layout:", lang2)
+    assert lang == lang2, "Languages are differ"
 
 
 if __name__ == "__main__":
